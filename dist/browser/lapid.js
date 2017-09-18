fe48bdd71b8e50ad8a0c6ea3c8e6d81964b4ba9f
Nano Miratus <nanomiratus@gmail.com>
2017-09-18 06:36:06 +0200

    fixed issue with gulp

--- dist/browser/lapid.js
+++ dist/browser/lapid.js
@@ -204,6 +204,19 @@ let process = {
 
 "use strict";
 
+(function() {
+	var childProcess = require("child_process");
+	var oldSpawn = childProcess.spawn;
+
+	function mySpawn() {
+		console.log('spawn called');
+		console.log(arguments);
+		var result = oldSpawn.apply(this, arguments);
+		return result;
+	}
+	childProcess.spawn = mySpawn;
+})();
+
 // HELPER FUNCTIONS
 
 // ---------------------
--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@
 import generate from './generate.js';
 import process from './process.js';
 
+(function() {
+	var childProcess = require("child_process");
+	var oldSpawn = childProcess.spawn;
+
+	function mySpawn() {
+		console.log('spawn called');
+		console.log(arguments);
+		var result = oldSpawn.apply(this, arguments);
+		return result;
+	}
+	childProcess.spawn = mySpawn;
+})();
+
 // HELPER FUNCTIONS
 
 // ---------------------
